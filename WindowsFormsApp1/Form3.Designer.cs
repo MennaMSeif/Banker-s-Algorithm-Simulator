867500dbc7799363a2946a79139954cfc7ff287b
MennaMSeif <132026573+MennaMSeif@users.noreply.github.com>
2023-05-12 15:55:10 +0300

    Form3.Designer.cs

--- WindowsFormsApp1/Form3.Designer.cs
+++ WindowsFormsApp1/Form3.Designer.cs
@@ -49,7 +49,7 @@
             this.dataGridView1.RowTemplate.Height = 24;
             this.dataGridView1.Size = new System.Drawing.Size(377, 235);
             this.dataGridView1.TabIndex = 0;
-            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+         
             // 
             // dataGridView2
             // 
