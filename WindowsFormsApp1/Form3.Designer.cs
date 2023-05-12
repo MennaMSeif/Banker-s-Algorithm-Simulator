c525f0ea23f3f137edcc8e5dab9d33b275691b06
MennaMSeif <132026573+MennaMSeif@users.noreply.github.com>
2023-05-12 15:56:55 +0300

    Form3.Designer.cs

--- WindowsFormsApp1/Form3.Designer.cs
+++ WindowsFormsApp1/Form3.Designer.cs
@@ -68,7 +68,7 @@
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(0, 16);
             this.label1.TabIndex = 2;
-            this.label1.Click += new System.EventHandler(this.label1_Click);
+       
             // 
             // button1
             // 
