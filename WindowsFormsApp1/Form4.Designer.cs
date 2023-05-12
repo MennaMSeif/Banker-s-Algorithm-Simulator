ce3031b3c7643e1bef9edc2861b8d653e33ed31c
MennaMSeif <132026573+MennaMSeif@users.noreply.github.com>
2023-05-12 15:57:48 +0300

    Form4.Designer.cs

--- WindowsFormsApp1/Form4.Designer.cs
+++ WindowsFormsApp1/Form4.Designer.cs
@@ -54,7 +54,6 @@
             this.label2.Size = new System.Drawing.Size(435, 37);
             this.label2.TabIndex = 1;
             this.label2.Text = "Banker\'s Algorithm Simulator";
-            this.label2.Click += new System.EventHandler(this.label2_Click);
             // 
             // button1
             // 
